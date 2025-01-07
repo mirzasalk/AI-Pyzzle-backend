d98f3b3145dda8c91d3cf277b0a7ef0b7f67dd6a
Mirza Salkovic <salkovic.mirza@gmail.com>
2025-01-07 17:47:24 +0100

    add /

--- puzzle/urls.py
+++ puzzle/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from puzzle import views
 
 urlpatterns = [
+    path('', lambda request: HttpResponse("Welcome to the backend!")),
     path('run-simulation/', views.RunSimulationView.as_view(), name='run_simulation'),   
 ]
